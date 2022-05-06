7e912895b33ea80b06b79831c3f50a484f7b3b68
Yoshiki217 <94679289+Yoshiki217@users.noreply.github.com>
2022-05-06 09:17:57 +0900

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,38 @@
+<?php
+  define( "DB_HOST", "localhost" );
+  define( "DB_USER", "admin03" );
+  define( "DB_PASS", "Admin!_03" );
+  define( "DB_NAME", "check_anpi" );  
+  //データベースに接続
+  $instance = new mysqli( DB_HOST, DB_USER , DB_PASS , DB_NAME);
+
+  if($instance->connect_error){
+    echo "Failed to connect to MySQL: ";
+    exit;
+  }
+  //データベースを選択
+  if (!$instance->select_db("check_anpi")) {
+    echo "データベース選択エラー" ;
+    exit ;
+  }
+
+  $instance->begin_transaction();
+  
+  $sql = "INSERT INTO user (id,password) value ('2222211','iguwei')";
+  
+  //SQLを実行
+  if (!$res = $instance->query($sql)) {
+    echo "SQL実行時エラー" ;
+    echo $sql;
+    $instance->rollback();
+    //データベースから切断
+    $instance->close() ;
+    exit;
+  }
+  
+  
+  $instance->commit();
+  
+  /* 接続を閉じます */
+  $instance->close();
+​?>
