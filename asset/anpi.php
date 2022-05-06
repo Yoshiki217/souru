24389044c2d53d4a880420701d52dc18ec450750
Yoshiki217 <94679289+Yoshiki217@users.noreply.github.com>
2022-05-06 12:01:53 +0900

    Update anpi.php

--- asset/anpi.php
+++ asset/anpi.php
@@ -3,8 +3,11 @@
   define( "DB_USER", "admin03" );
   define( "DB_PASS", "Admin!_03" );
   define( "DB_NAME", "check_anpi" );
+  define( "DB_CHAR", "utf8mb4" );
+  date_default_timezone_set("Asia/Tokyo");
   //データベースに接続
   $instance = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME );
+  $instance -> set_charset(DB_CHAR);
   if($instance->connect_error){
     echo "Failed to connect to MySQL: ";
     exit;
@@ -15,8 +18,8 @@
     exit ;
   }
   $instance->begin_transaction();
-  $time  = time();
-  $sql = "INSERT INTO anpi (id,time,status,text) value ('22s2211',$time,'1','大丈夫') ";
+  $date = date("Y:メトロ:d:H:i:s");
+  $sql = "INSERT INTO anpi (id,time,status,text) value ('222211',$date,'1','大丈夫') ";
   //SQLを実行
   if (!$res = $instance->query($sql)) {
     echo "SQL実行時エラー" ;
