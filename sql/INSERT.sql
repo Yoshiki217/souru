08633ec038704ef0d7501c4fddd789a051f0aeb4
fujishimaion <104334779+fujishimaion@users.noreply.github.com>
2022-05-09 16:47:05 +0900

    Add files via upload

---
+++ sql/INSERT.sql
@@ -0,0 +1,14 @@
+insert into belongs values(01,'営業');
+insert into belongs values(02,'総務');
+insert into belongs values(03,'人事');
+insert into divisions values(01,01,'営業一課');
+insert into divisions values(01,02,'営業二課');
+insert into divisions values(02,05,'経理課');
+insert into divisions values(02,06,'厚生課');
+insert into divisions values(03,11,'人事課');
+insert into divisions values(03,12,'教育課');
+insert into employee(id,name,bg_id,d_id)value(16010228,'小林晃年',01,01),
+(15010226,'石原梨花',01,02),(13020305,'平塚俊雄',02,05),(17021112,'関口隆',02,06),
+(20030702,'古橋紗奈',03,11),(21031201,'遠藤浩二',03,12),(18030704,'戸塚和美',03,11),
+(19011118,'日野紗奈',01,02),(16020412,'藤森加奈',02,06),(15030815,'近藤武',03,12);
+commit;
--- sql/createDB.sql
+++ sql/createDB.sql
@@ -6,39 +6,33 @@ GRANT SELECT, INSERT ON *.* TO admin03;
 
 use check_anpi;
 
-create table employee  (
-id char(8) unique not null primary key,
-name varchar(20) not null,
-bg_id char(2) not null,
-d_id char(2) not null
-);
-
-alter table employee add index employee_index(bg_id);
-alter table employee add index employee_index1(d_id);
-
-create table user  (
-id char(8) unique not null primary key,
-password varchar(100) not null,
-foreign key(id) references employee(id)
-);
-
 create table belongs (
 bg_id char(2) not null primary key,
-belong varchar(10) not null,
- foreign key(bg_id) references employee(bg_id)
+belong varchar(10) not null
 );
 
 create table  divisions (
 bg_id char(2) not null,
 d_id char(2) not null,
 division varchar(10) not null,
-primary key(bg_id,d_id),
-foreign key(bg_id) references employee(bg_id),
-foreign key(d_id) references employee(d_id)
+primary key(d_id),
+foreign key(bg_id) references belongs(bg_id)
+);
+
+create table employee  (
+id char(8) unique not null,
+name varchar(20),
+bg_id char(2) not null,
+d_id char(2) not null,
+password varchar(100),
+primary key(id),
+foreign key(bg_id) references belongs(bg_id),
+foreign key(d_id) references divisions(d_id)
 );
 
 create table anpi (
-id char(8) unique  not null primary key,
+id char(8) not null,
+name varchar(20),
 time time not null,
 status char(10) not null,
 text varchar(50),
