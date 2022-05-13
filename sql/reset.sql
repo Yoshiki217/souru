6a0000d5d62d3e61d80870cf370bbe953330781f
Yoshiki217 <94679289+Yoshiki217@users.noreply.github.com>
2022-05-13 12:13:08 +0900

    Update reset.sql

--- sql/reset.sql
+++ sql/reset.sql
@@ -3,10 +3,10 @@ drop user admin03;
 drop database check_anpi;
 
 CREATE USER 'admin03' IDENTIFIED BY 'Admin!_03';
-GRANT ALL ON *.* TO admin03;
-GRANT SELECT, INSERT ON *.* TO admin03;
-
 create database check_anpi;
+
+GRANT ALL ON check_anpi.* TO admin03;
+GRANT SELECT, INSERT ON *.* TO admin03;
 
 use check_anpi;
 
@@ -37,7 +37,7 @@ foreign key(d_id) references divisions(d_id)
 create table anpi (
 id char(8) not null,
 name varchar(20),
-time time not null,
+datetime datetime not null,
 status char(10) not null,
 text varchar(50),
 foreign key(id) references employee(id)
