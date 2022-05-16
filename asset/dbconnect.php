191806f7611f7d64084b281f6f73a410e2ea8b6a
Yoshiki217 <94679289+Yoshiki217@users.noreply.github.com>
2022-05-16 10:35:13 +0900

    Delete dbconnect.php

--- asset/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('C:\Sites\php\yosiki\core/config.php');
-
-$mysqli = new mysqli($host, $username, $password, $dbname);
-if ($mysqli->connect_error) {
-  error_log($mysqli->connect_error);
-  exit;
-}
-?>
